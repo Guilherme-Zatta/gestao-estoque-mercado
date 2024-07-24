e3c0d28c93e6a838bbd6f70fd35652c493cc7f8b
Guilherme-Zatta <guilhermezatta@hotmail.com>
2024-07-23 21:13:03 -0300

    23.07.24 - 21:12

--- src/main/java/br/senac/pr/gestao_estoque_mercado/movimentacao/MovimentacaoController.java
+++ src/main/java/br/senac/pr/gestao_estoque_mercado/movimentacao/MovimentacaoController.java
@@ -5,17 +5,14 @@ import java.util.List;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.senac.pr.gestao_estoque_mercado.movimentacao.dtos.CreateMovimentacaoDto;
-import br.senac.pr.gestao_estoque_mercado.movimentacao.dtos.UpdateMovimentacaoDto;
-import br.senac.pr.gestao_estoque_mercado.movimentacao.dtos.UpdateQtdeMovimentacaoDto;
+
 import br.senac.pr.gestao_estoque_mercado.movimentacao.impl.MovimentacaoServiceImpl;
 import br.senac.pr.gestao_estoque_mercado.shared.models.Movimentacao;
 
